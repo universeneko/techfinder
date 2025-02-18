8bfb5f73aa3d55a3a0b2efa5a92bf410e4d09eac
Neko <151251012+universeneko@users.noreply.github.com>
2025-02-17 23:12:23 -0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -130,7 +130,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), 
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
