385532c0d6d8ae16719f7020dc4588fd98af9fe9
Neko <151251012+universeneko@users.noreply.github.com>
2025-02-17 23:11:15 -0500

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -127,10 +127,10 @@ USE_TZ = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Configuración de archivos estáticos
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),  # Asegura que Django busca en la carpeta correcta
-]
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
