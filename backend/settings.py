c0109c58adec671dd44e4a04a1b54c8cb2377006
Neko <151251012+universeneko@users.noreply.github.com>
2025-02-17 23:02:35 -0500

    Update settings.py
    Hosts

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$_78tgwom3=dg25hn7wr!n3ojoh7$%k@y$)$2&js!g!wm_y%do
 DEBUG = False
 
 # settings.py
-ALLOWED_HOSTS = ['zippy-beauty.up.railway.app']  # Reemplaza X con tu IP local
+ALLOWED_HOSTS = ['zippy-beauty.up.railway.app', 'techfinder-production.up.railway.app', 'localhost', '127.0.0.1']  # Reemplaza X con tu IP local
 
 
 # Application definition
