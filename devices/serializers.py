bd8f2de06e52f99e96534ec6872e6b4b715c597d
Neko <151251012+universeneko@users.noreply.github.com>
2025-03-16 17:43:19 -0500

    Delete devices directory

--- devices/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Dispositivo
-
-class DispositivoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Dispositivo
-        fields = ['id', 'nombre', 'marca', 'tipo', 'fecha_lanzamiento', 'precio']
--- devices/urls.py
+++
@@ -1,9 +0,0 @@
-from xml.etree.ElementInclude import include
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-
-]
